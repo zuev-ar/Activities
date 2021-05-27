5651d153c9e57c64e601ae6fefb9decb71807c59
Arkadii Zuev <arkasha.zuev@icloud.com>
2021-05-27 15:40:40 +0500

    Initial Commit

---
+++ Activities/ActivitiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActivitiesApp.swift
+//  Activities
+//
+//  Created by Arkasha Zuev on 27.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct ActivitiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
